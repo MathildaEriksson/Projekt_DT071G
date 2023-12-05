ed744db598d23fa4992956f1e277f0542505e093
MathildaEriksson <tilla_89@hotmail.com>
2023-12-05 16:01:40 +0100

    Add enemy

---
+++ Projekt/Models/Enemy.cs
@@ -0,0 +1,32 @@
+﻿namespace Projekt.Models
+{
+    public class Enemy
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Speed { get; set; }
+
+        public Enemy(int x, int y, int width, int height, int speed)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Speed = speed;
+        }
+
+        public void Move(GameBoard gameBoard)
+        {
+            X += Speed;
+
+            // Vänd riktning vid kant av spelbrädet eller kollision med vägg etc.
+            if (X < 0 || X + Width > gameBoard.Width)
+            {
+                Speed = -Speed;
+            }
+        }
+    }
+
+}
